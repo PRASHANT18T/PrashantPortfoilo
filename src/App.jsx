78a8c6fe934a3594bfb3361302e0aefff8b68bc8
PRASHANT18T <prashant18t@gmail.com>
2025-08-19 16:34:33 +0530

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,15 @@ import About from "./pages/portfolioPages/AboutCard";
 import ProjectCards from "./pages/portfolioPages/ProjectCards";
 import Skills from "./pages/portfolioPages/Skills";
 import ContactForm from "./pages/portfolioPages/ContactForm";
-import Resume from "./Pages/portfolioPages/Resume";
+import Resume from "./pages/portfolioPages/Resume";
 import Project1 from "./pages/portfolioPages/projects/Project1";
 import Project2 from "./pages/portfolioPages/projects/Project2";
 import Project3 from "./pages/portfolioPages/projects/Project3";
 import Project4 from "./pages/portfolioPages/projects/Project4";
 import SkillLangs from "./pages/portfolioPages/SkillLangs";
 import AvailableForJobs from "./pages/portfolioPages/AvailableForJobs";
-import Freelance from "./Pages/portfolioPages/Freelance";
-import BlogsCart from "./Pages/portfolioPages/BlogsCart";
+import Freelance from "./pages/portfolioPages/Freelance";
+import BlogsCart from "./pages/portfolioPages/BlogsCart";
 import NotFound from "./pages/portfolioPages/NotFound";
 import BlogDetails from "./pages/portfolioPages/Blogs";
 
